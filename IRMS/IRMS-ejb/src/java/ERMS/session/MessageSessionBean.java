9566d9de193c616397fd079bd8a50928f7c1a94e
liuxian-nus <chrislx.nus@gmail.com>
2013-09-15 19:11:30 +0800

    internal messaging session bean

---
+++ IRMS/IRMS-ejb/src/java/ERMS/session/MessageSessionBean.java
@@ -0,0 +1,162 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ERMS.session;
+
+
+import ERMS.entity.MessageEntity;
+import ERMS.entity.ReceiverInfoEntity;
+import Exception.ExistException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+/**
+ *
+ * @author liuxian
+ */
+@Stateless
+@LocalBean
+public class MessageSessionBean {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    @EJB
+    ReceiverInfoSessionBean infoManager = new ReceiverInfoSessionBean();
+    
+    public MessageEntity getMessageById(Long messageId) throws ExistException{
+        System.out.println("MessageSessionBean--> get message with id" + messageId);
+        MessageEntity message = em.find(MessageEntity.class, messageId);
+        if(message == null) {
+            throw new ExistException("Message does not exist!");
+        }
+        return message;
+    }
+    
+    public void addPrivateMessage(Long senderId,Long receiverId,String title,String msg,String type)
+    {
+        System.out.println("MessageSessionBean-->creating a new message....");
+        MessageEntity message = new MessageEntity();
+        
+        ReceiverInfoEntity receiver = new ReceiverInfoEntity();
+        receiver.setDeleted(false);
+        receiver.setOpened(false);
+        receiver.setReceiverId(receiverId);
+        receiver.setSenderId(senderId);
+        
+        receiver.setMessage(message);
+        
+        List<ReceiverInfoEntity> infoList = new ArrayList<ReceiverInfoEntity>();
+        infoList.add(receiver);
+        
+        message.setRecInfo(infoList);
+        message.setTitle(title);
+        message.setContent(msg);
+        message.setType(type);
+        
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+ 
+        try {
+            
+            String sendTime = dateFormat.format(calendar.getTime());
+            message.setSendTime(sendTime);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("MessageSessionBean--> one new message sent: "+ msg + "--from person "+senderId);
+        em.persist(message);
+    }
+    
+    public void addSystemMessage(Long senderId,List<Long> idList,String title,String msg,String type)
+    {
+        System.out.println("MessageSessionBean-->generating a new system message....");
+        MessageEntity message = new MessageEntity();
+        
+        List<ReceiverInfoEntity> infoList = new ArrayList<ReceiverInfoEntity>();
+        for (int i=0;i<idList.size();i++){
+            ReceiverInfoEntity receiver = new ReceiverInfoEntity();
+            receiver.setDeleted(false);
+            receiver.setOpened(false);
+            receiver.setReceiverId(idList.get(i));
+            receiver.setSenderId(senderId);
+            receiver.setMessage(message);
+            
+            infoList.add(receiver);
+        }
+        
+        
+        message.setRecInfo(infoList);
+        message.setTitle(title);
+        message.setContent(msg);
+        message.setType(type);
+        
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+ 
+        try {
+            
+            String sendTime = dateFormat.format(calendar.getTime());
+            message.setSendTime(sendTime);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("MessageSessionBean--> one new system message sent: "+ msg);
+        em.persist(message);
+    }
+    
+    
+    public void editMessage(MessageEntity message)
+    {
+        em.merge(message);
+        System.out.println("MessageSessionBean--> message updated as " + message.getContent());
+        em.flush();
+    }
+    
+
+    public void removeMessage(Long MessageId, Long receiverId)throws ExistException
+    {
+        MessageEntity message = em.find(MessageEntity.class, MessageId);
+//        List<ReceiverInfo> infoList = message.getRecInfo();
+        if(message == null) {
+            throw new ExistException("Message does not exist!");
+        }
+        for (int i=0; i<message.getRecInfo().size(); i++) {
+            if (receiverId.equals(message.getRecInfo().get(i).getReceiverId())){
+                Long infoId = message.getRecInfo().get(i).getReceiveMsgId();
+                
+                message.getRecInfo().remove(i);
+                editMessage(message);
+                
+                infoManager.removeReceiverInfo(infoId);
+
+                break;
+            } 
+        }
+        
+        
+        if (message.getRecInfo().size()>0){
+            return;
+        }
+        else{
+            em.remove(message);
+        }        
+        
+    }
+    
+    public List<MessageEntity> getAllMessages(){
+        Query query = em.createQuery("SELECT s1 FROM MessageEntity s1");
+        return query.getResultList();
+    }
+    
+}
---
+++ IRMS/IRMS-ejb/src/java/ERMS/session/ReceiverInfoSessionBean.java
@@ -0,0 +1,131 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ERMS.session;
+
+import ERMS.entity.MessageEntity;
+import ERMS.entity.ReceiverInfoEntity;
+import Exception.ExistException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author liuxian
+ */
+@Stateless
+@LocalBean
+public class ReceiverInfoSessionBean {
+
+     @PersistenceContext
+    private EntityManager em;
+    
+    private MessageSessionBean mm;
+    
+    public ReceiverInfoEntity getReceiverInfoById(Long Id)throws ExistException
+    {
+        ReceiverInfoEntity info = em.find(ReceiverInfoEntity.class, Id);
+        if(info == null) {
+            throw new ExistException("ReceiverInfo does not exist!");
+        }
+        return info;
+    }
+    
+    
+    public void addReceiverInfo(ReceiverInfoEntity info)
+    {
+        System.out.println("ReceiverInfoSessionBean--> add ReceiverInfo");
+        em.persist(info);
+    }
+    
+    public void editEmployee(ReceiverInfoEntity info)
+    {
+        System.out.println("ReceiverInfoSessionBean--> edit ReceiverInfo");
+        em.merge(info);
+    }
+    
+
+    public void removeReceiverInfo(Long infoId) throws ExistException
+    {
+        ReceiverInfoEntity receiverInfo = em.find(ReceiverInfoEntity.class, infoId);
+        if(receiverInfo == null) {
+            throw new ExistException("ReceiverInfo does not exist!");
+        }
+         System.out.println("ReceiverInfoSessionBean--> remove ReceiverInfo");
+        em.remove(receiverInfo);
+    }
+    
+    public List<ReceiverInfoEntity> getAllReceiverInfo(){
+        Query query = em.createQuery("SELECT s1 FROM ReceiverInfoEntity s1");
+        return query.getResultList();
+    }
+    
+    public List<ReceiverInfoEntity> getRecInfoBySender(Long id){
+        Query query = em.createQuery("SELECT s1 FROM ReceiverInfoEntity s1 WHERE s1.senderId = :inRecId");
+        query.setParameter("inRecId", id);
+        List<ReceiverInfoEntity> recInfo = null;
+        
+        try{
+            recInfo = query.getResultList();
+        }
+        catch(NoResultException ex){
+            ex.printStackTrace();
+        }
+        
+        return recInfo;
+        
+    }
+    
+    public List<MessageEntity> getMessageBySender(Long id){
+        List<ReceiverInfoEntity> recInfo = getRecInfoBySender(id);
+        int i = 0;
+        List<MessageEntity> msg = new ArrayList<MessageEntity>();
+        while (recInfo.get(i) != null){
+            msg.add(recInfo.get(i).getMessage());
+        }
+        
+        return msg;
+    }
+
+    public List<ReceiverInfoEntity> getRecInfoByReceiver(Long id){
+        Query query = em.createQuery("SELECT s1 FROM ReceiverInfoEntity s1 WHERE s1.receiverId = :inRecId");
+        query.setParameter("inRecId", id);
+        List<ReceiverInfoEntity> recInfo = null;
+        
+        try{
+            recInfo = query.getResultList();
+        }
+        catch(NoResultException ex){
+            ex.printStackTrace();
+        }
+        
+        return recInfo;
+    }
+    
+    public List<MessageEntity> getMessageByReceiver(Long id){
+        List<ReceiverInfoEntity> recInfo = getRecInfoByReceiver(id);
+        List<MessageEntity> msg = new ArrayList<MessageEntity>();
+        for (int i=0;i<recInfo.size();i++) {
+            msg.add(recInfo.get(i).getMessage());
+        }
+        
+        return msg;
+    }
+
+    public MessageSessionBean getMm() {
+        return mm;
+    }
+
+    public void setMm(MessageSessionBean mm) {
+        this.mm = mm;
+    }
+    
+
+}
