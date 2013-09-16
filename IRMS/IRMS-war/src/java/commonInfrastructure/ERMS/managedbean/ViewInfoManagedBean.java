fc6d1ca93ed0cf58f17dbd8c7a29b050bdec4cf7
liuxian-nus <chrislx.nus@gmail.com>
2013-09-16 16:16:35 +0800

    viewInfoManagedBean debuged version

--- IRMS/IRMS-war/src/java/commonInfrastructure/ERMS/managedbean/ViewInfoManagedBean.java
+++ IRMS/IRMS-war/src/java/commonInfrastructure/ERMS/managedbean/ViewInfoManagedBean.java
@@ -51,7 +51,7 @@ public class ViewInfoManagedBean {
     
     public void saveChanges(ActionEvent event) throws ExistException
     {
-        em.updateEmployee(selectedEmployee.getEmployeeId(),selectedEmployee.getEmployeeName(),selectedEmployee.getEmployeeDob(),selectedEmployee.getEmployeeDepartment(),selectedEmployee.getEmployeeSchedule(),selectedEmployee.getEmployeeRole(),selectedEmployee.getEmployeeGender(),selectedEmployee.getEmployeePassword());
+        em.updateEmployee(selectedEmployee.getEmployeeId(),selectedEmployee.getEmployeeName(),selectedEmployee.getEmployeeDob(),selectedEmployee.getEmployeeDepartment(),selectedEmployee.getEmployeeSchedule(),selectedEmployee.getRoles(),selectedEmployee.getEmployeeGender(),selectedEmployee.getEmployeePassword());
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Changes saved.", ""));        
     }
 
