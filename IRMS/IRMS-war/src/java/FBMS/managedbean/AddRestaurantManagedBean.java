638933f74fe148e4948cbb69aadc1be7889bf349
Jieqiong <jean_lei@hotmail.com>
2013-09-18 17:45:44 +0800

    Jieqiong 18Sep AddRestaurantManagedBean

---
+++ IRMS/IRMS-war/src/java/FBMS/managedbean/AddRestaurantManagedBean.java
@@ -0,0 +1,59 @@
+
+package FBMS.managedbean;
+
+import FBMS.entity.RestaurantEntity;
+import FBMS.session.RestaurantSessionBeanRemote;
+import java.io.IOException;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.annotation.PostConstruct;
+import javax.faces.event.ActionEvent;
+
+
+/**
+ *
+ * @author Jieqiong
+ */
+@ManagedBean
+@RequestScoped
+public class AddRestaurantManagedBean implements Serializable{
+
+    @EJB
+    private RestaurantSessionBeanRemote restaurantSessionRemote;
+    private RestaurantEntity restaurant;
+    
+    @PostConstruct
+    public void init()
+    {
+        FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+    }
+    
+    public RestaurantEntity getRestaurant(){
+        return  restaurant;
+    }
+    
+    public void setRestaurant(RestaurantEntity restaurant){
+        this.restaurant=restaurant;
+    }
+    
+    public AddRestaurantManagedBean() {
+        restaurant=new RestaurantEntity();
+    }
+    
+    public void saveNewRestaurant (ActionEvent event) throws IOException{
+        try{
+            System.out.println("into AddRestaurantManagedBean");
+            restaurantSessionRemote.addRestaurant(restaurant);
+            System.out.println("out AddRestaurantManagedBean");
+        }catch (Exception e){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Error occurs when adding a new restaurant", ""));
+            return;
+        }
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "New restaurant saved.", ""));
+        restaurant=new RestaurantEntity();
+    }
+}
