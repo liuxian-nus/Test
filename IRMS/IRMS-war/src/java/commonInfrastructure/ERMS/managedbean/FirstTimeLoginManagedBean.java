13f3179c33a7312e6e093f000d1303c8150f3363
liuxian-nus <chrislx.nus@gmail.com>
2013-09-20 23:23:56 +0800

    bugged version FirstTimeLoginManagedBean

--- IRMS/IRMS-war/src/java/commonInfrastructure/ERMS/managedbean/FirstTimeLoginManagedBean.java
+++ IRMS/IRMS-war/src/java/commonInfrastructure/ERMS/managedbean/FirstTimeLoginManagedBean.java
@@ -12,10 +12,11 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.PhaseEvent;
 import javax.servlet.http.HttpServletRequest;
+
 /**
  *
  * @author liuxian
- * 
+ *
  * Not Finished!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  */
 @ManagedBean
@@ -24,58 +25,52 @@ public class FirstTimeLoginManagedBean {
 
     @EJB
     private EmployeeSessionBean employeeManagerSessionBean;
-    @EJB 
+    @EJB
     private EPasswordHashSessionBean passowordHashSessionBean;
-          
     private EmployeeEntity employee;
     private String passwordTemp;
     private String passwordTemp2;
-   
-   
-    public FirstTimeLoginManagedBean() {
 
+    public FirstTimeLoginManagedBean() {
     }
 
-
-   public void initView(PhaseEvent event){
-       if(employee==null)
-            employee=(EmployeeEntity)FacesContext.getCurrentInstance().getExternalContext().getFlash().get("employeeFirstTimeLogin");
-   }
-   
-   
-   public void doSubmit(ActionEvent event) throws IOException{
-        
-        if(getPasswordTemp().equals(getPasswordTemp2())){
-          employee.setEmployeePassword(passowordHashSessionBean.hashPassword(employee.getEmployeeId()+getPasswordTemp()));
-            
-               if(employee.isFirstTimeLogin()==true){
-                  
-                   employee.setIsFirstTimeLogin(false);
-                   //employeeManagerSessionBean.resetPassword(employee);
-               //persist!!!
-               }
-//               employee.setSecurityQuestion(securityQuestion);
-//               employee.setAns(ans);
-              // employeeManagerSessionBean.updateEmployee(employee);
-               
-                 HttpServletRequest request = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
-                 request.getSession().invalidate();
-                FacesContext.getCurrentInstance().getExternalContext().redirect("firstTimeLoginPwdChangeResult.xhtml");
-             
-            } else {
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Password Not Match!", ""));
-              
-            }
+    public void initView(PhaseEvent event) {
+        if (employee == null) {
+            employee = (EmployeeEntity) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("employeeFirstTimeLogin");
         }
-     public EmployeeSessionBean getEmployeeManagerSessionBean() {
+    }
+
+    public void doSubmit(ActionEvent event) throws IOException {
+
+        if (getPasswordTemp().equals(getPasswordTemp2())) {
+            if (employee.isFirstTimeLogin() == true) {
+                employee.setEmployeePassword(passowordHashSessionBean.hashPassword(employee.getEmployeeId() + getPasswordTemp()));
+                employee.setIsFirstTimeLogin(false);
+                employeeManagerSessionBean.updateEmployee(employee);
+            } 
+            employee.setSecurityQuestion(securityQuestion);
+            employee.setAns(ans);
+            employeeManagerSessionBean.updateEmployee(employee);
+            {
+                HttpServletRequest 
+            }
+            request = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+                 request.getSession().invalidate();
+            FacesContext.getCurrentInstance().getExternalContext().redirect("firstTimeLoginPwdChangeResult.xhtml");
+
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Password Not Match!", ""));
+
+        }
+    }
+
+    public EmployeeSessionBean getEmployeeManagerSessionBean() {
         return employeeManagerSessionBean;
     }
 
     public void setEmployeeManagerSessionBean(EmployeeSessionBean employeeManagerSessionBean) {
         this.employeeManagerSessionBean = employeeManagerSessionBean;
     }
-    
- 
 
     public EmployeeEntity getEmployee() {
         return employee;
@@ -100,6 +95,4 @@ public class FirstTimeLoginManagedBean {
     public void setPasswordTemp2(String passwordTemp2) {
         this.passwordTemp2 = passwordTemp2;
     }
-
-
 }
