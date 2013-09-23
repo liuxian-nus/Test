e3de3fa52f812f1eb3ca1c0e9c886fff5f9a2adb
ser3na <s.er3na.j@gmail.com>
2013-09-23 23:13:07 +0800

    RoleEntity delete auto generated ID

--- IRMS/IRMS-ejb/src/java/ERMS/entity/RoleEntity.java
+++ IRMS/IRMS-ejb/src/java/ERMS/entity/RoleEntity.java
@@ -9,8 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -22,7 +20,6 @@ import javax.persistence.OneToMany;
 public class RoleEntity implements Serializable {
      private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int roleId;
     private String roleName;
     
