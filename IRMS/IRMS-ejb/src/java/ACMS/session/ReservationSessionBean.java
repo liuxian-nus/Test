6c2772d7b066a9a3fbd03274b0a6a92326460808
liuxian-nus <chrislx.nus@gmail.com>
2013-09-23 21:54:40 +0800

    ReservationSessionBean.java added

---
+++ IRMS/IRMS-ejb/src/java/ACMS/session/ReservationSessionBean.java
@@ -0,0 +1,45 @@
+/*
+
+ */
+package ACMS.session;
+
+import ACMS.entity.ReservationEntity;
+import Exception.ExistException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author liuxian
+ */
+@Stateless
+public class ReservationSessionBean {
+
+    @PersistenceContext
+    private EntityManager em;
+    ReservationEntity reservation = new ReservationEntity();
+
+    public ReservationSessionBean() {
+    }
+
+    public List<ReservationEntity> getAllReservations() throws ExistException {
+        Query q = em.createQuery("SELECT re FROM ReservationEntity re");
+        List reservationList = new ArrayList<ReservationEntity>();
+        for (Object o : q.getResultList()) {
+            ReservationEntity re = (ReservationEntity) o;
+            reservationList.add(re);
+         }
+        if(reservationList.isEmpty())  throw new ExistException("Reservation database is empty!");
+        return reservationList;
+        }
+    
+    public ReservationEntity getReservationById(Long reservationId) throws ExistException {
+        reservation = em.find(ReservationEntity.class, reservationId);
+        if(reservation == null)throw new ExistException("Reservation does not exist!");
+        return reservation;
+        }
+    }
