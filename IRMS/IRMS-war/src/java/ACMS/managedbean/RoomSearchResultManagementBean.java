81ae2d0451de34e3f317128f2fc7e6e13b4f2535
liuxian-nus <chrislx.nus@gmail.com>
2013-09-24 22:50:17 +0800

    roomSearchResult

---
+++ IRMS/IRMS-war/src/java/ACMS/managedbean/RoomSearchResultManagementBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ACMS.managedbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author liuxian
+ */
+@ManagedBean
+@RequestScoped
+public class RoomSearchResultManagementBean {
+
+    /**
+     * Creates a new instance of RoomSearchResultManagementBean
+     */
+    public RoomSearchResultManagementBean() {
+    }
+}
