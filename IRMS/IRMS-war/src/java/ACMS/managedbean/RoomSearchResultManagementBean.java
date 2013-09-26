aff32fa55dfc2ac61c9f4ba5a7a628c966411778
liuxian-nus <chrislx.nus@gmail.com>
2013-09-27 00:04:49 +0800

    add content roomSearchResultManagedBean

--- IRMS/IRMS-war/src/java/ACMS/managedbean/RoomSearchResultManagementBean.java
+++ IRMS/IRMS-war/src/java/ACMS/managedbean/RoomSearchResultManagementBean.java
@@ -4,11 +4,14 @@
  */
 package ACMS.managedbean;
 
+import ACMS.entity.ReservationEntity;
 import ACMS.entity.RoomEntity;
 import java.io.Serializable;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
 
 /**
  *
@@ -18,8 +21,44 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class RoomSearchResultManagementBean implements Serializable{
 
-    private RoomEntity selectRoom;
+    private RoomEntity thisRoom;
     private List<RoomEntity> rooms;
+    private int roomId;
+    
     public RoomSearchResultManagementBean() {
     }
+    
+    public List<RoomEntity> getRooms() {
+        return rooms;
+    }
+    
+    public void setRooms(List<RoomEntity> rooms) {
+        this.rooms = rooms;
+    }
+    
+    public RoomEntity getThisRoom() {
+        return thisRoom;
+    }
+    
+    public void setThisRoom(RoomEntity thisRoom) {
+        this.thisRoom = thisRoom;
+    }
+    
+    public void initViewSelect(PhaseEvent event) {
+        thisRoom = (RoomEntity) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("thisRoom");
+    }
+    
+    public void intiViewAll(PhaseEvent event) {
+        rooms = (List<RoomEntity>) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("Rooms");
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+    
+    
 }
