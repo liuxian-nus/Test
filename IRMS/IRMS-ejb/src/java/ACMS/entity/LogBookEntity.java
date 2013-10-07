85b2b7431d73338a91eff5b81cceb49fd5882f3c
liuxian-nus <chrislx.nus@gmail.com>
2013-10-07 18:09:52 +0800

    new logbook entity & sessionBean

---
+++ IRMS/IRMS-ejb/src/java/ACMS/entity/LogBookEntity.java
@@ -0,0 +1,136 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ACMS.entity;
+
+import ERMS.entity.EmployeeEntity;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author liuxian
+ */
+@Entity
+public class LogBookEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long logBookId;
+    private String logTitle;
+    private String logText;
+    private String remark;
+    private int logShift;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date publishDate;
+    @ManyToOne(cascade={CascadeType.MERGE})
+    private EmployeeEntity logEmployee;
+
+    public LogBookEntity(){}
+    
+    public Long getId() {
+        return logBookId;
+    }
+
+    public void setId(Long id) {
+        this.logBookId = logBookId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (logBookId != null ? logBookId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof LogBookEntity)) {
+            return false;
+        }
+        LogBookEntity other = (LogBookEntity) object;
+        if ((this.logBookId == null && other.logBookId != null) || (this.logBookId != null && !this.logBookId.equals(other.logBookId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ACMS.entity.LogBookEntity[ id=" + logBookId + " ]";
+    }
+
+    public Long getLogBookId() {
+        return logBookId;
+    }
+
+    public void setLogBookId(Long logBookId) {
+        this.logBookId = logBookId;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public EmployeeEntity getEmployee() {
+        return logEmployee;
+    }
+
+    public void setEmployee(EmployeeEntity employee) {
+        this.logEmployee = employee;
+    }
+
+    public String getLogTitle() {
+        return logTitle;
+    }
+
+    public void setLogTitle(String logTitle) {
+        this.logTitle = logTitle;
+    }
+
+    public EmployeeEntity getLogEmployee() {
+        return logEmployee;
+    }
+
+    public void setLogEmployee(EmployeeEntity logEmployee) {
+        this.logEmployee = logEmployee;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getLogText() {
+        return logText;
+    }
+
+    public void setLogText(String logText) {
+        this.logText = logText;
+    }
+
+    public int getLogShift() {
+        return logShift;
+    }
+
+    public void setLogShift(int logShift) {
+        this.logShift = logShift;
+    }
+    
+}
---
+++ IRMS/IRMS-ejb/src/java/ACMS/session/LogBookSessionBean.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ACMS.session;
+
+import ACMS.entity.LogBookEntity;
+import Exception.ExistException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author liuxian
+ */
+@Stateless
+@LocalBean
+public class LogBookSessionBean {
+
+    @PersistenceContext
+    private EntityManager em;
+    LogBookEntity log = new LogBookEntity();
+
+    public LogBookSessionBean() {
+    }
+
+    public List<LogBookEntity> getAllLogs() throws ExistException {
+        Query q = em.createQuery("SELECT l FROM LogBookEntity l");
+        List logList = new ArrayList<LogBookEntity>();
+        for (Object o : q.getResultList()) {
+            LogBookEntity l = (LogBookEntity) o;
+            logList.add(l);
+        }
+        if (logList.isEmpty()) {
+            throw new ExistException("Reservation database is empty!");
+        }
+        return logList;
+    }
+
+    public List<LogBookEntity> getLogByShift(int logShift) throws ExistException {
+        Query query = em.createQuery("SELECT l FROM LogBookEntity l WHERE r.logShift = " + logShift);
+        System.err.println("getLogByShift: " + logShift);
+        //query.setParameter("employeeName", employeeName);
+        List<LogBookEntity> thisLogList = null;
+        thisLogList = query.getResultList();
+        if (thisLogList == null) {
+            throw new ExistException("This Shift does not have logs yet!");
+        }
+        return thisLogList;
+    }
+
+    public LogBookEntity addLog(LogBookEntity thisLog) {
+        em.persist(thisLog);
+        return thisLog;
+    }
+    
+    public LogBookEntity removeLog(Long logId) {
+        LogBookEntity thisLog = em.find(LogBookEntity.class, logId);
+        System.out.println("Remove this log" + logId);
+        em.remove(thisLog);
+        return thisLog;
+    }
+    
+    public LogBookEntity updateLog(Long logId, String title, String content, String remark) {
+        LogBookEntity thisLog = em.find(LogBookEntity.class, logId);
+        System.out.println("Update this log" + logId);
+        thisLog.setLogTitle(title);
+        thisLog.setLogText(content);
+        thisLog.setRemark(remark);
+        em.merge(thisLog);
+        return thisLog;
+    }
+}
