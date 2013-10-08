6311744e21f184121a913eb26c25b5d0bafac3c8
Jieqiong <jean_lei@hotmail.com>
2013-10-08 22:04:03 +0800

    attraction related

---
+++ IRMS/IRMS-ejb/src/java/ATMS/session/AttractionSessionBean.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ATMS.session;
+
+import ATMS.entity.AttractionEntity;
+import ATMS.entity.QuotaEntity;
+import Exception.ExistException;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Jieqiong
+ */
+@Stateless
+@LocalBean
+public class AttractionSessionBean {
+    @PersistenceContext(unitName = "IRMS-ejbPU")
+    private EntityManager em;
+    
+    AttractionEntity attr;
+    QuotaEntity quota=new QuotaEntity();
+    
+    public AttractionSessionBean(){
+    }
+    
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void addAttraction(AttractionEntity attr){
+        em.persist(attr);
+        return;
+    }
+    
+    public List<AttractionEntity> getAllAttractions()throws NoResultException{
+        Query query = em.createQuery("SELECT a FROM AttractionEntity a");
+        return query.getResultList();
+    }
+    
+    public void updateAttraction(AttractionEntity attr){
+        System.out.println("into AttractoinSessionBean: update Attraction");
+        quota=attr.getQuota();
+        quota.setRestQuota(quota.getMaxQuota());
+        attr.setQuota(quota);      
+        em.merge(attr);
+        em.flush();
+        System.out.println("updated");
+        return; 
+    }
+    
+    public boolean removeAttraction(String attrId)throws ExistException {
+        System.out.println("into removeAttraction");
+        attr = em.find(AttractionEntity.class, attrId);
+        if(attr == null) {
+            throw new ExistException("Attraction does not exist!");
+        }
+        em.remove(attr);
+        System.out.println("the attraction has been removed.");
+        return true;
+    } 
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+    
+
+
+
+}
---
+++ IRMS/IRMS-war/src/java/ATMS/managedBean/addAttractionManagedBean.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ATMS.managedBean;
+
+import ATMS.entity.AttractionEntity;
+import ATMS.entity.QuotaEntity;
+import ATMS.session.AttractionSessionBean;
+import java.io.IOException;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+/**
+ *
+ * @author Jieqiong
+ */
+@ManagedBean
+@RequestScoped
+public class addAttractionManagedBean {
+    @EJB
+    private AttractionSessionBean attractionSessionBean;
+    private AttractionEntity attr;
+    private QuotaEntity quota=new QuotaEntity();
+    
+    @PostConstruct
+    public void init()
+    {
+        FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+    }
+    
+    public addAttractionManagedBean() {
+        attr=new AttractionEntity();
+    }
+
+    public AttractionEntity getAttr() {
+        return attr;
+    }
+
+    public void setAttr(AttractionEntity attr) {
+        this.attr = attr;
+    }
+
+    public QuotaEntity getQuota() {
+        return quota;
+    }
+
+    public void setQuota(QuotaEntity quota) {
+        this.quota = quota;
+    }
+    
+    public void saveNewAttraction(ActionEvent event) throws IOException{
+        try{
+            System.out.println("into addAttractionManagedBean: saveNewAttraction");
+            quota=attr.getQuota();
+            quota.setRestQuota(quota.getMaxQuota());
+            attr.setQuota(quota);
+            System.out.println("attr name: "+attr.getAttrName());
+            attractionSessionBean.addAttraction(attr);
+            System.out.println("new attraction added");
+        }catch (Exception e){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Error occurs when adding a new attraction", ""));
+            return;
+        }
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "New Attraction Saved.", ""));
+        attr=new AttractionEntity();
+    }
+    
+    public void oneMore(ActionEvent event) throws IOException {
+        FacesContext.getCurrentInstance().getExternalContext().redirect("manageAttractions.xhtml");
+    }
+
+    
+    
+    
+    
+}
