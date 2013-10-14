48aad17a2eb5229d6ba35dfb69433d833d98203c
cookiewxy <cookiewxy@gmail.com>
2013-10-14 21:16:15 +0800

    link and overbooking layout added3

---
+++ IRMS/IRMS-ejb/src/java/SMMS/session/ContracteventSessionBean.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SMMS.session;
+
+import SMMS.entity.ContracteventEntity;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Cookie
+ */
+@Stateless
+@LocalBean
+public class ContracteventSessionBean {
+    @PersistenceContext(unitName = "IRMS-ejbPU")
+    private EntityManager em;
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+     ContracteventEntity newevent = new ContracteventEntity();
+     
+     @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public ContracteventEntity addContractevent(ContracteventEntity contractevent) {
+        em.persist(contractevent);
+        return contractevent;
+    }
+}
