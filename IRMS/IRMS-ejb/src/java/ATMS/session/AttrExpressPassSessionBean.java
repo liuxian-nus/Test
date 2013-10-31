33011d75e0c82276190987b95efe70263eea62b5
Jieqiong <jean_lei@hotmail.com>
2013-10-31 20:57:50 +0800

    EP done

--- IRMS/IRMS-ejb/src/java/ATMS/session/AttrExpressPassSessionBean.java
+++ IRMS/IRMS-ejb/src/java/ATMS/session/AttrExpressPassSessionBean.java
@@ -4,8 +4,17 @@
  */
 package ATMS.session;
 
+import ATMS.entity.AttrExpressPassEntity;
+import ATMS.entity.AttrTicketEntity;
+import Exception.ExistException;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -14,6 +23,64 @@ import javax.ejb.LocalBean;
 @Stateless
 
 public class AttrExpressPassSessionBean {
+    @PersistenceContext(unitName = "IRMS-ejbPU")
+    private EntityManager em;
+    
+    private AttrExpressPassEntity ep;
+    
+    public AttrExpressPassSessionBean(){
+        
+    }
+    
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void addEP(AttrExpressPassEntity ep){
+        em.persist(ep);
+        return;
+    }
+    
+    public List<AttrExpressPassEntity> getAllEPs(){
+        Query query = em.createQuery("SELECT ep FROM AttrExpressPassEntity ep");
+        return query.getResultList();
+    }
+    
+    public void updateEP(AttrExpressPassEntity ep){
+        System.out.println("into TicketSessionBean: update Attraction");   
+        em.merge(ep);
+        em.flush();
+        System.out.println("updated");
+        return; 
+    }
+    
+    public boolean removeEP(Long EPId)throws ExistException {
+        System.out.println("into removeTicket");
+        ep = em.find(AttrExpressPassEntity.class, EPId);
+        if(ep == null) {
+            throw new ExistException("EP does not exist!");
+        }
+        em.remove(ep);
+        System.out.println("the ep has been removed.");
+        return true;
+    } 
+    
+    public AttrExpressPassEntity getEPById(Long EPId) throws ExistException{
+        System.out.println("TicketSessionBean : getTicketById");
+        //System.out.println("ticketId passed in :"+ticketId);
+        ep=em.find(AttrExpressPassEntity.class, EPId);
+        if(ep==null){
+            throw new ExistException("ticket does not exist!");
+        }  
+        else{
+            System.out.println("ticket found");
+            return ep;
+        }
+        
+    }
+    
+    
+    public void persist(Object object) {
+        em.persist(object);
+    }
+    
 
 
 }
