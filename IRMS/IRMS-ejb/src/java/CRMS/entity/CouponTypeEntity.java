13cbd27e829cb106a8b64f06e8f2c0ff344659ca
Jieqiong <jean_lei@hotmail.com>
2013-11-06 23:13:34 +0800

    coupon type, set by manager, static

---
+++ IRMS/IRMS-ejb/src/java/CRMS/entity/CouponTypeEntity.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CRMS.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Administrator
+ */
+@Entity
+public class CouponTypeEntity implements Serializable {
+    /*This should be static entity created by manager
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long couponTypeId;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date cpStartDate;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date cpEndDate; //we can also not set date. assume the coupon is always valid
+    private String couponTypeRemarks;
+    private double discount; //less than one, for example, 0.9
+
+    public Long getCouponTypeId() {
+        return couponTypeId;
+    }
+
+    public void setCouponTypeId(Long couponTypeId) {
+        this.couponTypeId = couponTypeId;
+    }
+
+    public Date getCpStartDate() {
+        return cpStartDate;
+    }
+
+    public void setCpStartDate(Date cpStartDate) {
+        this.cpStartDate = cpStartDate;
+    }
+
+    public Date getCpEndDate() {
+        return cpEndDate;
+    }
+
+    public void setCpEndDate(Date cpEndDate) {
+        this.cpEndDate = cpEndDate;
+    }
+
+    public String getCouponTypeRemarks() {
+        return couponTypeRemarks;
+    }
+
+    public void setCouponTypeRemarks(String couponTypeRemarks) {
+        this.couponTypeRemarks = couponTypeRemarks;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (couponTypeId != null ? couponTypeId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CouponTypeEntity)) {
+            return false;
+        }
+        CouponTypeEntity other = (CouponTypeEntity) object;
+        if ((this.couponTypeId == null && other.couponTypeId != null) || (this.couponTypeId != null && !this.couponTypeId.equals(other.couponTypeId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CRMS.entity.CouponTypeEntity[ id=" + couponTypeId + " ]";
+    }
+    
+}
---
+++ IRMS/IRMS-ejb/src/java/CRMS/session/CouponTypeSessionBean.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CRMS.session;
+
+import CRMS.entity.CouponTypeEntity;
+import Exception.ExistException;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Jieqiong
+ */
+@Stateless
+@LocalBean
+public class CouponTypeSessionBean {
+    @PersistenceContext(unitName = "IRMS-ejbPU")
+    private EntityManager em;
+    private CouponTypeEntity couponType;
+    
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    public CouponTypeSessionBean(){
+        couponType=new CouponTypeEntity();
+    }
+    
+   @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void addCouponType(CouponTypeEntity ct){
+        System.out.println("CouponTypeSessionBean : addCouponType");
+        em.persist(ct);
+        return;
+    }
+    
+   @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void updateCouponType(CouponTypeEntity ct){
+        System.out.println("CouponTypeSessionBean : updateCouponType");
+        em.merge(ct);
+        em.flush();
+        return;
+    }
+   
+   public List<CouponTypeEntity> getAllCouponTypes(){
+       System.out.println("CouponTypeSessionBean : getAllCouponTypes");
+        Query query = em.createQuery("SELECT ct FROM CouponTypeEntity ct");
+        return query.getResultList();
+    }
+    
+    public CouponTypeEntity getCouponTypeById(Long id) throws ExistException{
+        System.out.println("CouponTypeSessionBean : getCouponTypeById");
+        couponType=em.find(CouponTypeEntity.class, id);
+        if(couponType==null) throw new ExistException("Coupon type doesn't exist");
+        else return couponType;
+    }
+    
+    public void removeCouponType(Long id)throws ExistException {
+        System.out.println("CouponTypeSessionBean :removeCouponType");
+        couponType=em.find(CouponTypeEntity.class, id);
+        if(couponType==null) throw new ExistException("Coupon type doesn't exist");
+        em.remove(couponType);
+        System.out.println("the ticket has been removed.");
+        return;
+    }
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+    
+
+}
