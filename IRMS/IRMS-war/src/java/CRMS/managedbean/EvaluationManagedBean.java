d888e6c66e688f58d79d97ab943064936977bb14
ser3na <s.er3na.j@gmail.com>
2013-11-12 15:04:27 +0800

    minor bug fixed for size of wallet

--- IRMS/IRMS-war/src/java/CRMS/managedbean/EvaluationManagedBean.java
+++ IRMS/IRMS-war/src/java/CRMS/managedbean/EvaluationManagedBean.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -35,43 +37,61 @@ public class EvaluationManagedBean {
     private MemberEntity member;
     private MemberEntity selectedMember;
     private PieChartModel pieModel;
+    private Double recency;
+    private Double frequency;
+    private Double monetary;
+    private int modelNumber;
+    private String memberEmail;
+    private boolean calculateStatus;
+    private Integer RFMValue;
+    private List<MemberEntity> memberList = new ArrayList<MemberEntity>();
 
     /**
      * Creates a new instance of EvaluationManagedBean
      */
-    public EvaluationManagedBean() {
+    public EvaluationManagedBean() throws ExistException {
         member = new MemberEntity();
     }
-    
+
     public List<MemberEntity> getMemberSizeOfWallet() throws ExistException {
-        List<MemberEntity> memberList = new ArrayList<MemberEntity>();
+        memberList = new ArrayList<MemberEntity>();
         List<MemberEntity> tempList = memberSessionBean.getAllMembers();
         Iterator<MemberEntity> itr = tempList.iterator();
         while (itr.hasNext()) {
             member = itr.next();
             evaluationSessionBean.calculateSizeOfWallet(member.getMemberEmail());
         }
-        memberList = tempList;
+        memberList = memberSessionBean.getAllMembers();
         return memberList;
     }
 
     public void memberShareOfWallet(ActionEvent event) throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().getFlash().put("memberS", selectedMember);
-        System.err.println("1. "+selectedMember.getMemberEmail());
+        System.err.println("1. " + selectedMember.getMemberEmail());
         FacesContext.getCurrentInstance().getExternalContext().redirect("shareOfWallet.xhtml");
     }
-    
-     public void createShareOfWallet(PhaseEvent event) {
+
+    public void createShareOfWallet(PhaseEvent event) {
         member = (MemberEntity) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("memberS");
-        System.err.println("2. "+member.getMemberEmail());
+        System.err.println("2. " + member.getMemberEmail());
         pieModel = new PieChartModel();
 
-        pieModel.set("Hotel", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(),"hotel"));
-        pieModel.set("Shopping Mall", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(),"shopping mall"));
-        pieModel.set("Entertainment Show", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(),"entertainment show"));
-        pieModel.set("Food and Beverage", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(),"food and beverage"));
-        pieModel.set("Attraction", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(),"attraction"));
-        pieModel.set("Convention Center", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(),"convention center"));
+        pieModel.set("Hotel", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(), "hotel"));
+        pieModel.set("Shopping Mall", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(), "shopping mall"));
+        pieModel.set("Entertainment Show", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(), "entertainment show"));
+        pieModel.set("Food and Beverage", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(), "food and beverage"));
+        pieModel.set("Attraction", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(), "attraction"));
+        pieModel.set("Convention Center", evaluationSessionBean.calculateShareOfWallet(member.getMemberEmail(), "convention center"));
+    }
+
+    public void saveRFMParameter(ActionEvent event) throws IOException, ExistException {
+        evaluationSessionBean.setRFMParameter(recency, frequency, monetary, modelNumber);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "New RFM Parameters saved.", ""));
+        FacesContext.getCurrentInstance().getExternalContext().redirect("rfmValue.xhtml");
+    }
+
+    public void calculateRFM(ActionEvent event) throws IOException, ExistException {
+        RFMValue = evaluationSessionBean.calculateRFMValue(memberEmail, modelNumber);
     }
 
     public MemberSessionBean getMemberSessionBean() {
@@ -112,5 +132,61 @@ public class EvaluationManagedBean {
 
     public void setSelectedMember(MemberEntity selectedMember) {
         this.selectedMember = selectedMember;
+    }
+
+    public Double getRecency() {
+        return recency;
+    }
+
+    public void setRecency(Double recency) {
+        this.recency = recency;
+    }
+
+    public Double getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(Double frequency) {
+        this.frequency = frequency;
+    }
+
+    public Double getMonetary() {
+        return monetary;
+    }
+
+    public void setMonetary(Double monetary) {
+        this.monetary = monetary;
+    }
+
+    public int getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(int modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+
+    public boolean isCalculateStatus() {
+        return calculateStatus;
+    }
+
+    public void setCalculateStatus(boolean calculateStatus) {
+        this.calculateStatus = calculateStatus;
+    }
+
+    public Integer getRFMValue() {
+        return RFMValue;
+    }
+
+    public void setRFMValue(Integer RFMValue) {
+        this.RFMValue = RFMValue;
     }
 }
