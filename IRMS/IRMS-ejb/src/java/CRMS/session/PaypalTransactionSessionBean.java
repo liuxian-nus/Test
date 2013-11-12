39cc263803e7f1c8c6e1d6b0340635b48cd658bd
diana-wang-qq <dianawangqq@gmail.com>
2013-11-12 18:28:22 +0800

    Mod

--- IRMS/IRMS-ejb/src/java/CRMS/session/PaypalTransactionSessionBean.java
+++ IRMS/IRMS-ejb/src/java/CRMS/session/PaypalTransactionSessionBean.java
@@ -4,8 +4,14 @@
  */
 package CRMS.session;
 
+import CRMS.entity.PaypalTransactionEntity;
+import java.util.Iterator;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -14,8 +20,33 @@ import javax.ejb.LocalBean;
 @Stateless
 @LocalBean
 public class PaypalTransactionSessionBean {
+    @PersistenceContext(unitName = "IRMS-ejbPU")
+    private EntityManager em;
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
 
+    public PaypalTransactionEntity getPaypalTranByToken(String token)
+    {
+        Query q = em.createQuery("SELECT PTE FROM PAYPALTRANSACTIONENTITY PTE");
+        List <PaypalTransactionEntity> list = q.getResultList();
+        Iterator <PaypalTransactionEntity> itr = list.iterator();
+        PaypalTransactionEntity result = new PaypalTransactionEntity();
+        
+        while(itr.hasNext())
+        {
+            PaypalTransactionEntity current = itr.next();
+            if(current.getToken().equalsIgnoreCase(token))
+            {
+                result = current;
+                break;
+            }
+        }
+        
+        return result;
+    }
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
 }
