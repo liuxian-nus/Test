72367c2ac2b3122871f78cb5aeb6547985b4fcaf
cookiewxy <cookiewxy@gmail.com>
2013-11-13 00:10:18 +0800

    room edit in cell

--- IRMS/IRMS-war/src/java/ACMS/managedbean/RoomPriceManagedBean.java
+++ IRMS/IRMS-war/src/java/ACMS/managedbean/RoomPriceManagedBean.java
@@ -9,6 +9,7 @@ import ACMS.session.RoomPriceSessionBean;
 import Exception.ExistException;
 import java.io.IOException;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -23,10 +24,42 @@ import org.primefaces.event.CellEditEvent;
 @ManagedBean
 @ViewScoped
 public class RoomPriceManagedBean {
+
     @EJB
     private RoomPriceSessionBean roomPriceSessionBean;
     private List<RoomPriceEntity> roomPriceList;
     private RoomPriceEntity thisRoomPrice;
+    private double newPrice;
+
+    /**
+     * Creates a new instance of RoomPriceManagedBean
+     */
+    @PostConstruct
+    public void init() {
+        roomPriceList = roomPriceSessionBean.getAllRoomPrices();
+    }
+
+    public RoomPriceManagedBean() {
+    }
+
+    public List<RoomPriceEntity> getAllRoomPrices() throws ExistException, IOException {
+        System.err.println("in getAll rooms");
+        return roomPriceSessionBean.getAllRoomPrices();
+    }
+
+    public void onCellEdit(CellEditEvent event) {
+        System.out.println("in editing roolslslslsls");
+        Object oldValue = event.getOldValue();
+        Object newValue = event.getNewValue();
+        System.out.println("here in editing price" + newValue);
+        if (newValue != null && !newValue.equals(oldValue)) {
+            System.out.println("here in editing price" + thisRoomPrice.getPriceType());
+            thisRoomPrice.setPrice(Double.valueOf((String) newValue));
+            roomPriceSessionBean.updatePrice(thisRoomPrice);
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Room Price updated", "Original: " + oldValue + ", New:" + newValue);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
 
     public List<RoomPriceEntity> getRoomPriceList() {
         return roomPriceList;
@@ -51,28 +84,5 @@ public class RoomPriceManagedBean {
     public void setNewPrice(double newPrice) {
         this.newPrice = newPrice;
     }
-    private double newPrice;
-
-    /**
-     * Creates a new instance of RoomPriceManagedBean
-     */
-    public RoomPriceManagedBean() {
-    } 
-      public List<RoomPriceEntity> getAllRoomPrices() throws ExistException, IOException {
-        System.err.println("in getAll rooms");
-        return roomPriceSessionBean.getAllRoomPrices();
-    }
-      
-      public void onCellEdit(CellEditEvent event) {  
-        System.out.println("in editing roolslslslsls");
-        Object oldValue = event.getOldValue();  
-        Object newValue = event.getNewValue();  
-          
-        if(newValue != null && !newValue.equals(oldValue)) {  
-            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Room Price updated", "Original: " + oldValue + ", New:" + newValue);  
-            FacesContext.getCurrentInstance().addMessage(null, msg);  
-        }  
-    }  
-      
-     //public doUpdate(ActionEvent event)
+    //public doUpdate(ActionEvent event)
 }
