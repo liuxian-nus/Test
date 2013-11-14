3b7b947a13626534c3eaaf547588f5007327b177
cookiewxy <cookiewxy@gmail.com>
2013-11-14 19:20:52 +0800

    Long text

--- IRMS/IRMS-ejb/src/java/CRMS/entity/MemberMessageEntity.java
+++ IRMS/IRMS-ejb/src/java/CRMS/entity/MemberMessageEntity.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -32,6 +33,7 @@ public class MemberMessageEntity implements Serializable {
     private Long messageId;
     private String messageTitle;
     private String messageCategory;
+    @Lob
     private String messageContent;
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date messageSentDate;
